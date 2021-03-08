071eb6715637117c4988b4e5c6f6b297fe8ca3a0
Shekhar <nk.shekhar24@gmail.com>
2021-03-08 13:21:46 +0530

    Cleanup

--- example/setup.py
+++ example/setup.py
@@ -5,15 +5,14 @@ from Cython.Distutils import build_ext
 data_dir = pkg_resources.resource_filename("autowrap", "data_files")
 include_dir = os.path.join(data_dir, "autowrap")
 
-ext = Extension("py_libcpp_test",
-                sources = ['py_libcpp_test.cpp'],
+ext = Extension("py_int_holder",
+                sources = ['py_int_holder.cpp'],
                 language="c++",
                 include_dirs = [include_dir, data_dir],
                )
 
 setup(cmdclass={'build_ext':build_ext},
-      name="py_libcpp_test",
+      name="py_int_holder",
       version="0.0.1",
       ext_modules = [ext]
-)
-
+     )
